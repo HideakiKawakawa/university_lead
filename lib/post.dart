6a40134f878c2cc24a4455c55d60d49be62554f6
HideakiKawashima <hideshimabaseball0531@ezweb.ne.jp>
2024-07-14 14:32:13 +0900

    Add  PostText

--- lib/post.dart
+++ lib/post.dart
@@ -14,11 +14,13 @@ class _PostPageState extends State<PostPage> {
   final TextEditingController _textEditingController3 = TextEditingController();
   final TextEditingController _textEditingController4 = TextEditingController();
   final TextEditingController _textEditingController5 = TextEditingController();
+  final TextEditingController _textEditingController6 = TextEditingController();
   String _postText1 = '';
   String _postText2 = '';
   String _postText3 = '';
   String _postText4 = '';
   String _postText5 = '';
+  String _postText6 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +76,15 @@ class _PostPageState extends State<PostPage> {
               labelText: 'その理由* ',
             ),
           ),
+          TextField(
+            controller: _textEditingController6,
+            enabled: true,
+            style: const TextStyle(color: Colors.black),
+            obscureText: false,
+            decoration: const InputDecoration(
+              labelText: '受験生に一言* ',
+            ),
+          ),
           const SizedBox(height: 20), // ボタンとテキストフィールドの間にスペースを追加
           ElevatedButton(
             onPressed: () {
@@ -85,9 +96,10 @@ class _PostPageState extends State<PostPage> {
                 _postText3 = _textEditingController3.text;
                 _postText4 = _textEditingController4.text;
                 _postText5 = _textEditingController5.text;
+                _postText6 = _textEditingController6.text;
               });
               String postTexts =
-                  '$_postText1\n$_postText2\n$_postText3\n$_postText4\n$_postText5';
+                  '$_postText1\n$_postText2\n$_postText3\n$_postText4\n$_postText5\n$_postText6';
 
               // MyApp の状態を更新する（ここでは Navigator を使用して画面を戻す）
               Navigator.of(context).pop(postTexts); // ここでは _postText を引数として渡す
